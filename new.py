34a05e6259ad48d3b96cf698460f9c227d0fae5a
Priyanshu-Datta <priyanshudatta80@gmail.com>
2025-11-30 00:31:23 +0530

    first python code

---
+++ new.py
@@ -0,0 +1,19 @@
+print("my name is priyanshu")          
+# single line comand 
+a = 23 
+print(type(a))
+b='name'
+print(type(b))
+c=int(input("enter a number"))
+print(c)
+
+d = 23 + 73
+e = 23 * 73
+print(d,e)
+
+# # '''  ,jmjgujgujgujg 
+# uygauludwglbaJJDLUJASBLJVLJSLJVS'''
+i = int(input("enter i"))
+j= int(input("enter j"))
+f = (i)&(j)
+print (f)
